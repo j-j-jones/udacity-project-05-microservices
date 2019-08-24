2a61c991b8b6f9e9b5707be98ba1920d9f5adbfe
J. Jonathan (J.J.) Jones <jj.jones@live.com>
2019-08-23 23:28:26 -0400

    Add files via upload

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,11 +5,12 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
-dockerpath=jjones/udacity
+dockerpath=jjones/udacity:latest
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-kubectl run development --image=$dockerpath --port=80
+#kubectl run deployment boston-housing --image=$dockerpath --port=80
+kubectl run api-housing --image=$dockerpath --port=8000
 
 
 # Step 3:
@@ -19,5 +20,6 @@ kubectl get pods --all-namespaces
 
 # Step 4:
 # Forward the container port to a host
-kubectl expose deployment development --type=LoadBalancer --port=8080
+#kubectl expose deployment api-housing --type=LoadBalancer --port=8000
+kubectl port-forward deployment.apps/api-housing 8000:80
 
